e4d5f92e52b415c929fe878386e48847efbddef3
tiancj <cj.tian.seu@gmail.com>
2018-09-27 16:21:41 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Change Wallpaper for linuxmint
+
+for Gnome Shell refer [mojave.xml](https://gist.github.com/trongthanh/7d632e90687e1bc219e1f3262d337702) and [How To Use macOS Mojave’s Dynamic Wallpaper on Linux](https://www.omgubuntu.co.uk/2018/06/macos-mojave-dynamic-background-linux)
+
+1. clone repository
+
+```bash
+git clone https://github.com/tiancj/chwallpaper
+```
+
+2. download mojave dynamic wallpaper and extract it to `~/Pictures/mojave_dynamic`
+
+```bash
+wget https://files.rb.gd/mojave_dynamic.zip
+```
+
+3. run program
+
+```bash
+cd chwallpaper
+python3 chwallpaper.py
+```
+
---
+++ chwallpaper.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+# 
+# Download and unzip Mojave dynamic background here: https://files.rb.gd/mojave_dynamic.zip 
+# gsettings set org.cinnamon.desktop.background picture-uri "file:///home/tiancj/Pictures/mojave_dynamic/mojave_dynamic_16.jpeg"
+# mojave_dynamic_[0:16].jpeg
+
+import os
+import sys
+import time
+
+if not os.path.exists(os.path.expanduser("~/Pictures/mojave_dynamic")):
+    print("mojave_dynamic folder doesn't exist, exit")
+    sys.exit(0)
+
+pic_info = [
+    [2, 10800, "mojave_dynamic_16.jpeg"],
+    [5, 3600, "mojave_dynamic_1.jpeg"],
+    [6, 3600, "mojave_dynamic_2.jpeg"],
+    [7, 3600, "mojave_dynamic_3.jpeg"],
+    [8, 3600, "mojave_dynamic_4.jpeg"],
+    [9, 3600, "mojave_dynamic_5.jpeg"],
+    [10, 3600, "mojave_dynamic_6.jpeg"],
+    [11, 7200, "mojave_dynamic_7.jpeg"],
+    [13, 3600, "mojave_dynamic_8.jpeg"],
+    [14, 3600, "mojave_dynamic_9.jpeg"],
+    [15, 3600, "mojave_dynamic_10.jpeg"],
+    [16, 3600, "mojave_dynamic_11.jpeg"],
+    [17, 3600, "mojave_dynamic_12.jpeg"],
+    [18, 7200, "mojave_dynamic_13.jpeg"],
+    [20, 10800, "mojave_dynamic_14.jpeg"],
+    [23, 10800, "mojave_dynamic_15.jpeg"],
+]
+
+BASE_FOLDER = os.path.expanduser('~/Pictures/mojave_dynamic')
+
+while True:
+    # get current hour
+    t = time.localtime()
+    hour = t.tm_hour
+    for info in pic_info:
+        if info[0] <= hour < info[0] + int(info[1]/3600):
+            # found
+            image = 'file://' + os.path.join(BASE_FOLDER, info[2])
+            command = 'gsettings set org.cinnamon.desktop.background picture-uri ' + image
+            print("command: ", command)
+            os.system(command)
+
+            # sleep
+            time_to_sleep = info[1] - (hour-info[0])*3600 - t.tm_min*60
+            print("sleeping: {} seconds".format(time_to_sleep))
+            time.sleep(time_to_sleep)
+            break
+
